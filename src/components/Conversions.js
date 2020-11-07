ed8badb60e3d3571bb6bc7046bff051eb779d312
Nevers <nickyeversdesign@gmail.com>
2020-11-06 22:17:46 -0800

    Troubleshoot value updating lifecycle

--- src/components/Conversions.js
+++ src/components/Conversions.js
@@ -1,30 +1,33 @@
 import React, {useState} from 'react'
 
 
-export default function Conversion(props) { 
+export default function Conversion(props) {
+    console.log('props', props)
+    let saltAmount = props.selectedFerment*props.amount
 
-    
-let saltAmount = props.selectedFerment*props.amount
-console.log(props.ferments.label)
+    console.log('selectedFerment conversion', props.selectedFerment)
+
 
     return (
         <div className="ferment">
         <div className="first-select">
            <input className="ferment-input" type="number" value={props.amount} onChange={props.handleChange}/>
-           {props.ferments.label === 'Bell Peppers' ? (<input className="ferment-input" type="number" value={props.amount} onChange={props.handleChange}/>) : null}
+
+           {/* {props.ferments.label === 'Bell Peppers' ? (<input className="ferment-input" type="number" value={props.amount} onChange={props.handleChange}/>) : null} */}
+
             <p> kg of </p>
-           <select 
-      value={props.selectedFerment}
-      onChange={props.onChangeFerment}>
-           {props.ferments.map((option, index) => (
-               <option key={index} value ={(option.value * 1000).toFixed(1)}>{option.label}</option>
-               ))}  
-           </select>
+            <select
+                value={props.selectedFerment}
+                onChange={props.onChangeFerment}>
+            {props.ferments.map((option, index) => (
+                <option key={index} value={props.selectedFerment.label === option.label ? (props.selectedFerment.value * 1000).toFixed(1) : (option.value * 1000).toFixed(1)}>
+                    {props.selectedFerment.label === option.label ? props.selectedFerment.label : option.label}</option>
+                ))}
+            </select>
            </div>
            <p className="equals">is</p>
            <p> <span>{saltAmount} </span>grams of salt</p>
-               
-           
+
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ import Miso from '../components/Miso'
 
 export default function App() {
 
-  
+
   const ferments = [
     {label: "Kimchi", value: .02},
     {label: "Sauerkraut", value: .02},
@@ -28,35 +28,27 @@ export default function App() {
     {label: "Green Beans", value: .02},
   ]
   // const [fermentationOptions, setFermentationOptions] = useState([])
-  const [ferment, setFerment] = useState(ferments[0].value*1000)
-  const [selectedFerment, setSelectedFerment] = useState(null)
+  // const [ferment, setFerment] = useState(ferments[0].value * 1000)
+
+  const [selectedFerment, setSelectedFerment] = useState(ferments[0])
+  console.log('selectedFerment index', selectedFerment)
+
   const [amount, setAmount] = useState(1)
-  const [salinity, setSalinity] = useState(ferments[0].value*1000)
 
-console.log(ferment)
-
-
-// useEffect(() => {
-//       // setFermentationOptions([data, ...salinity])
-//       const firstFerment =ferments[0]
-//       setFerment(ferments)
-//       setSelectedFerment(firstFerment)
-//       setSalinity(firstFerment.value)      
-//     }, [])
-    //does this need to change everytime a new ferment is selected?
-    
-    // const onFermentSelect = ferment => {
-    //   setSelectedFerment(ferment)
-    // }
+  const [salinity, setSalinity] = useState(ferments[0].value * 1000)
 
     function handleSaltChange(e) {
         setAmount(e.target.value)
         //doesn't change
-        setSalinity(ferment * amount)        
-      }  
+        setSalinity(ferment * amount)
+      }
 
       function handleFermentChange(e) {
-        setFerment(e.target.value)
+        console.log('selectedFerm before', selectedFerment)
+
+        setSelectedFerment(e.target.value)
+
+        console.log('selectedFerm after', selectedFerment)
       }
 
 console.log(salinity)
@@ -67,16 +59,30 @@ console.log(salinity)
    <Conversions
 
         ferments={ferments}
-        selectedFerment={ferment}
-        onChangeFerment={handleFermentChange} //e.target.value = whatever the value of our select option 
+        selectedFerment={selectedFerment}
+        onChangeFerment={handleFermentChange} //e.target.value = whatever the value of our select option
         //doesn't work
-        onChangeSalinity={e => setSalinity(e.target.value * amount)} 
+        onChangeSalinity={e => setSalinity(e.target.value * amount)}
         handleChange={handleSaltChange}
         amount={amount}
-        salinity={salinity}  
+        salinity={salinity}
         />
 
    </>
   );
 }
 
+
+
+// useEffect(() => {
+//       // setFermentationOptions([data, ...salinity])
+//       const firstFerment =ferments[0]
+//       setFerment(ferments)
+//       setSelectedFerment(firstFerment)
+//       setSalinity(firstFerment.value)
+//     }, [])
+    //does this need to change everytime a new ferment is selected?
+
+    // const onFermentSelect = ferment => {
+    //   setSelectedFerment(ferment)
+    // }
